8257f11eb32f4163b55153ee5417037a6827f693
Camposm97 <camposm1205@gmail.com>
2019-08-16 21:06:01 -0400

    Setting up packages

--- src/app/App.java
+++ src/app/App.java
@@ -1,12 +1,23 @@
-package src.app;
+package app;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+	private BorderPane root;
+	
+	@Override
+	public void init() {
+		root = new BorderPane();
+	}
+	
 	@Override
 	public void start(Stage stage) {
+		Scene scene = new Scene(root);
 		stage.setTitle("Pazaak");
+		stage.setScene(scene);
 		stage.show();
 	}
 	
