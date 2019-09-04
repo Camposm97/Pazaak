a55fe562e345980e89fa73658d0ccdc07ebf0ac5
Camposm97 <camposm1205@gmail.com>
2019-09-04 09:53:18 -0400

    Modified App.java and Demo.java

--- src/app/App.java
+++ src/app/App.java
@@ -1,33 +1,19 @@
 package app;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import layout.MainMenu;
-import util.ImgUtil;
+import stage.MyStage;
 
 public class App extends Application {
 	public static final String TITLE = "Pazaak v1.0";
-	private MainMenu root;
-	
-	@Override
-	public void init() {
-		root = new MainMenu();
-	}
 	
 	@Override
 	public void start(Stage stage) {
-		Scene scene = new Scene(root);
-		stage.getIcons().add(ImgUtil.loadImg(ImgUtil.ICON_PAZAAK));
-		stage.setTitle(TITLE);
-		stage.setWidth(1600);
-		stage.setHeight(900);
-		stage.setResizable(false);
-		stage.setScene(scene);
+		stage = new MyStage();
 		stage.show();
 	}
 	
 	public static void main(String[] args) {
-		launch(args);
+		Application.launch(args);
 	}
 }
--- src/app/Demo.java
+++ src/app/Demo.java
@@ -2,10 +2,6 @@ package app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DataFormat;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import model.Card;
@@ -15,30 +11,8 @@ public class Demo extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		Card pc1 = new PazaakCard();
-		pc1.setOnDragDetected(e -> {
-			System.out.println("onDragDetected");
-            /* allow any transfer mode */
-            Dragboard db = pc1.startDragAndDrop(TransferMode.ANY);
-            /* put a string on dragboard */
-            ClipboardContent content = new ClipboardContent();
-            content.put(new DataFormat("0"), pc1);
-            db.setContent(content);
-            e.consume();
-		});
-		Card pc2 = new PazaakCard(null);
-		pc2.setOnDragOver(e -> {
-			System.out.println("onDragOver");
-            
-            /* accept it only if it is  not dragged from the same node 
-             * and if it has a string data */
-            if (e.getGestureSource() != pc2 &&
-                    e.getDragboard().hasContent(new DataFormat("0"))) {
-                /* allow for both copying and moving, whatever user chooses */
-                e.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-            }
-            
-            e.consume();
-		});
+		Card pc2 = new Card();
+
 		HBox hBox = new HBox(10);
 		hBox.getChildren().addAll(pc1, pc2);
 		Scene scene = new Scene(hBox);
