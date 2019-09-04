2cce6cad38fddbc382703149a7bfc05784764d34
Camposm97 <camposm1205@gmail.com>
2019-09-04 09:53:38 -0400

    Modified MainMenu.java

--- src/layout/MainMenu.java
+++ src/layout/MainMenu.java
@@ -5,7 +5,6 @@ import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -16,31 +15,31 @@ import util.Web;
 
 public class MainMenu extends BorderPane {
 	public MainMenu() {
-		ImageView iv = ImgUtil.loadImgV(ImgUtil.ICON_PAZAAK, 0.5);
 		Label lblTitle = new Label(App.TITLE);
 		lblTitle.setPadding(FXUtil.DEFAULT_INSETS);
 		lblTitle.setFont(Font.font(64));
-		lblTitle.setGraphic(iv);	
+		lblTitle.setGraphic(ImgUtil.loadImgV(ImgUtil.ICON_PAZAAK, 0.5));	
 		setTop(new StackPane(lblTitle));
-		setCenter(loadBtns());
+		setCenter(loadBoxOfBtns());
 	}
 	
-	private VBox loadBtns() {
-		final Font font = Font.font(16);
+	private VBox loadBoxOfBtns() {
 		Button btStart = new Button("Start");
 		btStart.setOnAction(e -> { getScene().setRoot(new SideDeckChooser()); });
-		btStart.prefWidthProperty().bind(widthProperty().multiply(0.25));
-		btStart.setFont(font);
 		Button btHelp = new Button("How to Play");
 		btHelp.setOnAction(e -> { Web.openThisRepo(); });
-		btHelp.prefWidthProperty().bind(widthProperty().multiply(0.25));
-		btHelp.setFont(font);
 		Button btQuit = new Button("Quit");
 		btQuit.setOnAction(e -> { Platform.exit(); });
-		btQuit.prefWidthProperty().bind(widthProperty().multiply(0.25));
-		btQuit.setFont(font);
+		editBtns(btStart, btHelp, btQuit);
 		VBox vBox = FXUtil.loadVBox(btStart, btHelp, btQuit);
 		vBox.setAlignment(Pos.CENTER);
 		return vBox;
+	}
+	
+	private void editBtns(Button...btns) {
+		for (Button bt : btns) {
+			bt.prefWidthProperty().bind(widthProperty().multiply(0.25));
+			bt.setFont(Font.font(16));
+		}
 	}
 }
