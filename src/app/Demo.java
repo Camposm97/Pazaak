ad1247f6317fc493adcd8010e7d3b9528c73ef81
Camposm97 <camposm1205@gmail.com>
2019-09-05 20:51:27 -0400

    Modified Demo.java

--- src/app/Demo.java
+++ src/app/Demo.java
@@ -3,14 +3,19 @@ package app;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import layout.game_table.ScoreToken;
+import layout.game_table.GameBoard;
+import model.DeckPicker;
+import model.PazaakCard;
 
 public class Demo extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
-		Scene scene = new Scene(new ScoreToken());
+		PazaakCard[] deck = DeckPicker.getRandomDeck();
+		Scene scene = new Scene(new GameBoard(deck));
 		stage.setScene(scene);
+		stage.setFullScreen(true);
 		stage.show();
+		
     }
 
     public static void main(String[] args) {
