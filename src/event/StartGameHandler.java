81fd162b6ddde1dc576ebbb54fd23302127b5e81
Camposm97 <camposm1205@gmail.com>
2019-09-05 20:49:21 -0400

    Modified StartGameHandler.java

--- src/event/StartGameHandler.java
+++ src/event/StartGameHandler.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyCode;
 import javafx.stage.StageStyle;
 import layout.DeckPickerPane;
-import layout.game_table.GameTable;
+import layout.game_table.GameBoard;
 import model.DeckPicker;
 import model.PazaakCard;
 import stage.PauseStage;
@@ -25,7 +25,7 @@ public class StartGameHandler implements EventHandler<ActionEvent> {
 		PazaakCard[] deck = pane.getDeckPicker().getPickedCards();
 		if (deck != null) {
 			Scene scene = pane.getScene();
-			scene.setRoot(new GameTable(deck));
+			scene.setRoot(new GameBoard(deck));
 			scene.setOnKeyPressed(f -> {
 				if (f.getCode() == KeyCode.ESCAPE)
 					new PauseStage(scene.getWindow());
